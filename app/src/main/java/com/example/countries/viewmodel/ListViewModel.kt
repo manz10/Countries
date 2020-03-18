114daedc65bebb4cefb6ecbae5f5e548bcc4a77b
manzworld <manz.maharjan10@gmail.com>
2020-03-18 18:29:42 +1100

    View Layout Implemented

--- app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countries/viewmodel/ListViewModel.kt
@@ -1,7 +1,37 @@
 package com.example.countries.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.countries.model.Country
 
 class ListViewModel: ViewModel(){
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()   //error message
+    val loading = MutableLiveData<Boolean>()        //check if loading
+
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries(){
+        val mockData: List<Country> = listOf(Country(countryName = "CountryA"),
+            Country(countryName = "CountryB"),
+            Country(countryName = "CountryC"),
+            Country(countryName = "CountryD"),
+            Country(countryName = "CountryE"),
+            Country(countryName = "CountryF"),
+            Country(countryName = "CountryG"),
+            Country(countryName = "CountryH"),
+            Country(countryName = "CountryI")
+        )
+
+        countryLoadError.value = false   //no error in loading data
+        loading.value = false
+
+        countries.value = mockData
+
+    }
+
 
 }
